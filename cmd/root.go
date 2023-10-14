c6d22a8fb841134231b437bdfac54c0baafd1746
Agung Pratama <agp.csui08@gmail.com>
2023-10-14 18:59:57 +0700

    Add simple docs

--- cmd/root.go
+++ cmd/root.go
@@ -18,13 +18,17 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "hugo-to-ghost",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "A simple CLI that helps migrating contents from Hugo (Markdown) to Ghost",
+	Long: `A simple CLI that helps migrating contents from Hugo (Markdown) to Ghost.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+This CLI program will output json file that can later be imported to Ghost Import functionality (via Admin dashboard).
+
+For example:
+- hugo-to-ghost --contentDir ./content/english/post > ghost-content.json
+	to output the JSON file containing all Hugo's posts that conformed with the Ghost Importing Content format:
+- hugo-to-ghost --contentDir ./content/english/post --staticContentPrefixChange "/img/uploads/,/content/images/hugo/"
+	to change the image assets in Hugo (that previously located in /img/uploads/ path) to /content/images/hugo folder inside the ghost installation
+`,
 	Run: cmdRun,
 }
 
